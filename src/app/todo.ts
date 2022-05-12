b4f313d7f65612b22e622a5533ebce93b6d4670a
nghia191 <mai.nam.nghia.bunbu@gmail.com>
2022-05-12 16:35:26 +0700

    fix contructor

--- src/app/todo.ts
+++ src/app/todo.ts
@@ -5,12 +5,11 @@
 // }
 
 export class Todo {
-  id;
-  name;
-  completed;
-  constructor(id: number, name: string, completed: boolean) {
-    (this.id = id), (this.name = name), (this.completed = completed);
-  }
+  constructor(
+    public id: number,
+    public name: string,
+    public completed: boolean
+  ) {}
 }
 
 export function getRandomId() {
